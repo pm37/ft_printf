cdaf95a493a0452405867e4705b03e64aaaa9590
Pierre MICHAUD <pimichau@e2r2p10.42.fr>
2019-02-20 19:11:11 +0100

    ftoa

--- lib/ft_ftoa.c
+++ lib/ft_ftoa.c
@@ -11,29 +11,28 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-char	*ft_get_f_dec(double nb, int dec)
+#include <stdio.h>
+#include <limits.h>
+char	*get_dec_part(long double nb, int prec)
 {
-	char		*str;
-	char		*str_tmp;
-	int			i;
+	char		*dec_part;
+	char		*dec_tmp;
 	int			len;
 
 	str = ft_strnew(0);
-	i = dec;
-	while (i >= 1)
+	while (prec >= 1)
 	{
 		nb = (nb - (long long)nb) * 10;
-		str_tmp = ft_llitoa(nb);
-		str = ft_strjoin(str, str_tmp);
-		i--;
+		dec_tmp = ft_itoa(nb);
+		dec_part = ft_strjoin(dec_part, dec_tmp);
+		prec--;
 	}
-	len = ft_strlen(str);
+	len = ft_strlen(dec_part);
 	if (len - 2 >= 0)
-		if (str[len - 1] > '4')
-			str[len - 2]++;
-	str[len - 1] = '\0';
-	return (str);
+		if (dec_part[len - 1] > '4')
+			dec_part[len - 2]++;
+	dec_part[len - 1] = '\0';
+	return (dec_part);
 }
 
 char	*ft_get_f_dec2(double nb, int dec)
@@ -53,52 +52,37 @@ char	*ft_get_f_dec2(double nb, int dec)
 	}
 	return (str);
 }
-char	*ft_get_f_int(double nb)
+char	*get_int_part(long double nb)
 {
-	char	*str;
-	char	*str_tmp;
-	int		i;
-	double	nbr;
+	//long long n;
+	int				i;
+	char			*int_part;
 
-	nbr = nb;
 	i = 0;
-	str = ft_strnew(0);
-	while (nb > 1)
+	while (nb > 9223372036854775807.0)
 	{
-		nb /= 2;
+		nb /= 10;
 		i++;
 	}
-	//str = ft_llitoa(nb);
-	//printf("\ni vaut :%d\n", i);
-	//printf("nb vaut : %f", nb);	
-	//nbr = nbr - ((long long)nb * 100.0);
-	//printf("nbr vaut : %f", nbr);	
-	//nb *= 100;
-	//printf("nb vaut : %f", nb);	
-	while (i--)
-	{
-		nb = nb * 2;
-		str_tmp = ft_llitoa(nb);
-		str = ft_strjoin(str, str_tmp);
-		nb = nb - (long)nb;
-	}
-	//str = ft_strjoin(str, ft_get_f_dec2(nb, i));
-	
-	return (str);
+	int_part = ft_llitoa((long long)nb);
+	if (i)
+		int_part = ft_strjoin(int_part, get_dec_part(nb, i + 1));
+	return (int_part);
 }
-char	*ft_ftoa(double nb, int dec)
+char	*ft_lftoa(long double nb, int prec)
 {
-	char				*str;
-	char				*str_int;
+	char				*dec_part;
+	char				*int_part;
 	int					neg;
 	int					i;
 
 	i = 1;
-	if (!dec)
-		return (ft_strdup(ft_itoa((int)nb)));
+	if (!prec)
+		return (ft_llitoa((long long)nb));
 	neg = (nb < 0) ? 1 : 0;
-	str = ft_get_f_dec(nb, dec + 1);
+	dec_part = get_dec_part(nb, prec + 1);
 //	printf("\n et un de plus : %f\n", nb);
-	str_int = ft_get_f_int(nb);
-	return (ft_strjoin(str_int, ft_strjoin(".", str)));
+	int_part = get_int_part(nb);
+	printf("int_part: %s\ndec_part: %s\n", int_part, dec_part);
+	return (ft_strjoin(int_part, ft_strjoin(".", dec_part)));
 }
