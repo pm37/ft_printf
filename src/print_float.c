0b58601532ca2cb08ab8cef7a386da58fa115128
Benjamin WAN NAN WO <bwan-nan@e1r5p17.42.fr>
2019-02-26 18:00:53 +0100

    bugs with flags on floats fixed

--- src/print_float.c
+++ src/print_float.c
@@ -6,20 +6,19 @@
 /*   By: pimichau <pimichau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 13:56:18 by pimichau          #+#    #+#             */
-/*   Updated: 2019/02/26 17:34:12 by pimichau         ###   ########.fr       */
+/*   Updated: 2019/02/26 17:42:30 by bwan-nan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	print_float(t_conv *conv)
+static void		print_flags_before(t_conv *conv)
 {
 	if (FLAG.space && ((ft_strequ(RESULT, "nan") && !FLAG.less) || !SIGN)
 	&& !ft_strequ(RESULT, "inf"))
 	{
 		conv->ret += write(1, " ", 1);
-		if ((ft_strequ(RESULT, "nan") && !FLAG.less)
-		|| (!ft_strequ(RESULT, "nan") && FLAG.less))
+		if ((!ft_strequ(RESULT, "nan") && FLAG.less))
 			--conv->width;
 	}
 	if (conv->width > LEN + P_DIFF + SIGN && (!FLAG.zero || EDGE) && !FLAG.less)
@@ -35,6 +34,11 @@ void	print_float(t_conv *conv)
 	if (conv->width > LEN + P_DIFF + SIGN && FLAG.zero && !FLAG.less && !EDGE)
 		while (--conv->width > LEN + P_DIFF + SIGN)
 			conv->ret += write(1, "0", 1);
+}
+
+void			print_float(t_conv *conv)
+{
+	print_flags_before(conv);
 	conv->ret += write(1, RESULT, LEN);
 	while (P_DIFF--)
 		conv->ret += write(1, "0", 1);
