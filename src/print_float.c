ff5a49b29f5a613c5f63357543c82d405a012dda
Benjamin WAN NAN WO <bwan-nan@student.42.fr>
2019-02-28 17:48:39 +0100

    print_flags removed tmp, stupid # flag with 0 precision on floats

--- src/print_float.c
+++ src/print_float.c
@@ -6,7 +6,7 @@
 /*   By: pimichau <pimichau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 13:56:18 by pimichau          #+#    #+#             */
-/*   Updated: 2019/02/28 12:37:30 by pimichau         ###   ########.fr       */
+/*   Updated: 2019/02/28 17:45:53 by bwan-nan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,8 @@ void			print_float(t_conv *conv)
 	}
 	print_flags_before(conv);
 	RET += write(1, RESULT, LEN);
+	if (FLAG.sharp && PREC == 0)
+		RET += write(1, ".", 1);
 	while (P_DIFF--)
 		RET += write(1, "0", 1);
 	if (WIDTH > LEN + P_DIFF + SIGN && FLAG.less)
